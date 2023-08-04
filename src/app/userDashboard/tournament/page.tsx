ec26945d315de1f829d2423dde59accd9a485677
Divyanshdubey001 <divyanshdubey@technogetic.com>
2023-08-04 19:00:07 +0530

    user dashboard

--- src/app/userDashboard/tournament/page.tsx
+++ src/app/userDashboard/tournament/page.tsx
@@ -15,15 +15,24 @@ function Tournament() {
         <div className={styles.abcd}>
           <div className={styles.sidebar_wrapper}>
             <Navbar />
-            <div>
-              <h1>Upcoming Matches</h1>
-              <h6>Dashboard/Upcoming Matches</h6>
+            <div className={styles.content}>
+              Upcoming Matches
+              <h1>Dashboard/Upcoming Matches</h1>
+              <div className={styles.input_desc}>
+              <Input
+              placeholder="Search by fees"
+              ></Input>
             </div>
+            </div>
+            
           </div>
-          <div>
-            <div>
-              <div>
-                <img src='../assests/userdashboardbg.svg' alt='userdashboardbg'></img>
+          <div className={styles.room_wrapper}>
+            <div className={styles.room_container}>
+              <div className={styles.imgSection}>
+                <img src='../assests/userdashboardbg.webp' alt='userdashboardbg' className={styles.wrapperimg}></img>
+              </div>
+              <div className={styles.content_body}>
+                <h2>BGMI SQUAD MATCH </h2>
               </div>
             </div>
           </div>
