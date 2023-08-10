887b4e34cbcd561aac9518f48e4ad697121c79c1
Expert developer <expertdeveloper@Experts-MacBook-Pro.local>
2023-08-11 00:44:33 +0530

    updated service

--- src/services/api/apiServices.tsx
+++ src/services/api/apiServices.tsx
@@ -1,9 +1,10 @@
+import { configData } from "@/utils/config";
 import axios, { AxiosInstance } from "axios";
 
 console.log("object check base url ", process.env.NEXT_PUBLIC_API_BASE_URL, process.env.NEXT_PUBLIC_API_BASE_VER)
 
 const apiServices: AxiosInstance = axios.create({
-    baseURL: `http://localhost:5000/api/v1/`,
+    baseURL: `${configData.api.baseURL}/api/${configData.api.ver}/`,
     headers: {
         'Content-Type': 'application/json; charset=UTF-8',
     },
