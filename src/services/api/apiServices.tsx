48ed015d723864029f528e4eaad8e9598d62d1bb
Expert developer <expertdeveloper@Experts-MacBook-Pro.local>
2023-08-11 00:48:44 +0530

    removed console

--- src/services/api/apiServices.tsx
+++ src/services/api/apiServices.tsx
@@ -1,7 +1,6 @@
 import { configData } from "@/utils/config";
 import axios, { AxiosInstance } from "axios";
 
-console.log("object check base url ", process.env.NEXT_PUBLIC_API_BASE_URL, process.env.NEXT_PUBLIC_API_BASE_VER)
 
 const apiServices: AxiosInstance = axios.create({
     baseURL: `${configData.api.baseURL}/api/${configData.api.ver}/`,
