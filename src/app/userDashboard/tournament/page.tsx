62bbe1ea870aa36071926b04321fa6db04a93422
developerAkshay1 <akshay.kumar@technogetic.com>
2023-08-12 18:52:28 +0530

    make it a responsive design

--- src/app/userDashboard/tournament/page.tsx
+++ src/app/userDashboard/tournament/page.tsx
@@ -1,17 +1,16 @@
-'use client'
-import React, { useEffect, useState } from 'react';
+'use client';
+import React, {useEffect, useState} from 'react';
 import styles from '../../../styles/Dashboard.module.scss';
-import { Navbar } from '../../../Components/Navbar/Navbar';
-import {Button} from 'technogetic-iron-smart-ui'
-import { decodeJWt } from '@/utils/globalfunctions';
+import {Navbar} from '../../../Components/Navbar/Navbar';
+import {Button} from 'technogetic-iron-smart-ui';
+import {decodeJWt} from '@/utils/globalfunctions';
 import Image from 'next/image';
 import sendRequest from '@/services/auth/auth_All_Api';
 
-export interface IAppProps { }
+export interface IAppProps {}
 
 function Tournament() {
-
-  const [alldata,setData] = useState<any>([]);
+  const [alldata, setData] = useState<any>([]);
   const [lastTournament, setLastTournament] = useState<any>(null);
   const [allTournaments, setAllTournaments] = useState<any>(null);
   const [gameName, setMatchName] = useState<any>('');
@@ -21,130 +20,179 @@ function Tournament() {
   const [regMatches, setRegMatches] = useState<any>('');
 
   const getAllTournaments = async () => {
-    const token:any = localStorage.getItem('jwtToken');
-    const decodedToken: any = decodeJWt(token)
+    const token: any = localStorage.getItem('jwtToken');
+    const decodedToken: any = decodeJWt(token);
     const tournamentResponse = await sendRequest('api/v1/room/rooms', {
       method: 'GET',
-      headers: {'Authorization': `Bearer ${token}`  }
+      headers: {Authorization: `Bearer ${token}`},
     });
-    setData(tournamentResponse.data[0].rooms);  
+    setData(tournamentResponse.data[0].rooms);
 
-    const registeredMatches = await sendRequest('api/v1/team/register-room/'+decodedToken.userId, {
-      method: 'GET',
-      headers: {'Authorization': `Bearer ${token}`  }
-    });
+    const registeredMatches = await sendRequest(
+      'api/v1/team/register-room/' + decodedToken.userId,
+      {
+        method: 'GET',
+        headers: {Authorization: `Bearer ${token}`},
+      },
+    );
     setRegMatches(registeredMatches.data.rooms);
-  } 
+  };
 
-  console.log('setRegMatches_________',regMatches)
+  console.log('setRegMatches_________', regMatches);
 
-  useEffect(()=>{
-    getAllTournaments(); 
-  },[])
+  useEffect(() => {
+    getAllTournaments();
+  }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     setLastTournament(alldata[alldata.length - 1]);
-    setAllTournaments(alldata?.slice(0,3));
-  },[alldata])
+    setAllTournaments(alldata?.slice(0, 3));
+  }, [alldata]);
 
-  useEffect(()=>{
+  useEffect(() => {
     setMatchName(lastTournament?.gameName);
     setGameType(lastTournament?.gameType);
     setMapType(lastTournament?.mapType);
     setVersion(lastTournament?.version);
-  },[lastTournament])
+  }, [lastTournament]);
 
-  const updateMainData = (gname:any, gType:any, mType:any, vType:any) =>{
+  const updateMainData = (gname: any, gType: any, mType: any, vType: any) => {
     setMatchName(gname);
     setGameType(gType);
     setMapType(mType);
     setVersion(vType);
-  }
-
-  console.log('allTournaments_________',allTournaments);
-  
+  };
   return (
     <>
       <div className={styles.main_container}>
         <div className={styles.abcd}>
           <div className={styles.sidebar_wrapper}>
             <Navbar />
-            
-            <div className={styles.content}>
 
+            <div className={styles.content}>
               <div className={styles.dashboard}>
                 <span className={styles.head_desc}>Upcoming Matches</span>
-                <h1 className={styles.subhead_desc}>Dashboard/Upcoming Matches</h1>
-
+                <h1 className={styles.subhead_desc}>
+                  Dashboard/Upcoming Matches
+                </h1>
               </div>
               {/* <div className={styles.sort_btn}>
                 <Image src='../assests/sort_button.svg' alt='sortButton' className={styles.wrapperimg} width={100} height={100}/>
               </div> */}
-
             </div>
-
-          
-          <div className={styles.room_wrapper}>
-            <div className={styles.room_container}>
-              <div className={styles.registeredmatches}>
-                <div className={styles.imgSection}>
-                  <Image src='../assests/userdashboardbg.svg' alt='userdashboardbg' className={styles.wrapperimg} width={100} height={100}/>
+            <div className={styles.room_wrapper}>
+              <div className={styles.room_container}>
+                <div className={styles.registeredmatches}>
+                  <div className={styles.imgSection}>
+                    <Image
+                      src="../assests/userdashboardbg.svg"
+                      alt="userdashboardbg"
+                      className={styles.wrapperimg}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+                  <span className={styles.register_match}>
+                    Registered Matches
+                  </span>
                 </div>
-                <span className={styles.register_match}>Registered  Matches</span>
-              </div>
-              <div className={styles.squad_match}>
-                <span className={styles.register_match}>{gameName}</span>
-                <span className={styles.winning_prize}>Time : 02/08/2023 at 06:00pm</span>
+                <div className={styles.squad_match}>
+                  <span className={styles.register_match}>{gameName}</span>
+                  <span className={styles.winning_prize}>
+                    Time : 02/08/2023 at 06:00pm
+                  </span>
 
-                <div className={styles.winnings}>
+                  <div className={styles.winnings}>
+                    <div>
+                      <span className={styles.winning_prize}>
+                        WINNING PRIZE
+                      </span>
+                      <span className={styles.survival_content}>
+                        {' '}
+                        Last Survival: 60
+                      </span>
+                    </div>
 
-                  <div>
-                    <span className={styles.winning_prize}>WINNING PRIZE</span>
-                    <span className={styles.survival_content}> Last Survival: 60</span>
+                    <div>
+                      <span className={styles.winning_prize}> Entry FEES</span>
+                      <span className={styles.survival_content}>
+                        {' '}
+                        50{' '}
+                        <span className="rs_logo">
+                          <Image
+                            src="../assests/rupee-icon.svg"
+                            alt="rupeeIcon"
+                            width={12}
+                            height={12}
+                          />
+                        </span>
+                      </span>
+                    </div>
                   </div>
 
-                  <div>
-                    <span className={styles.winning_prize}> Entry FEES</span>
-                    <span className={styles.survival_content}> 50 <span className='rs_logo'><Image src='../assests/rupee-icon.svg' alt='rupeeIcon' width={12} height={12}/></span></span>
+                  <div className={styles.winnings}>
+                    <div>
+                      <span className={styles.winning_prize}>TYPE</span>
+                      <span
+                        className={styles.tvm_font}
+                        style={{color: 'rgba(255, 214, 0, 1)'}}
+                      >
+                        {gameType}
+                      </span>
+                    </div>
 
-                  </div>
-                </div>
+                    <div>
+                      <span className={styles.winning_prize}>VERSION</span>
+                      <span
+                        className={styles.tvm_font}
+                        style={{color: 'rgba(255, 214, 0, 1)'}}
+                      >
+                        {version}
+                      </span>
+                    </div>
 
-                <div className={styles.winnings}>
-
-                  <div>
-                    <span className={styles.winning_prize}>TYPE</span>
-                    <span className={styles.tvm_font} style={{ color: 'rgba(255, 214, 0, 1)' }}>{gameType}</span>
-                  </div>
-
-                  <div>
-                    <span className={styles.winning_prize}>VERSION</span>
-                    <span className={styles.tvm_font} style={{ color: 'rgba(255, 214, 0, 1)' }}>{version}</span>
-                  </div>
-
-                  <div>
-                    <span className={styles.winning_prize}>MAP</span>
-                    <span className={styles.tvm_font} style={{ color: 'rgba(255, 122, 0, 1)' }}>{mapType}</span>
-                  </div>
-                </div>
-
-                <div className={styles.winnings}>
-                  <div className={styles.spot_line}>
-                    <span className={styles.bar_font}> Only 30 spots Left</span>
-                    <span className={styles.bar_font}>20/50</span>
-                  </div>
-                  <Button className={styles.join_button}> Join</Button>
-                </div>
-
-
-                <div className={styles.winnings}>
-                  <div className={styles.slidebar}>
-                    {
-                      allTournaments &&  allTournaments.map((e:any)=><img src='../assests/cards.svg' alt='slides'  onClick={() => updateMainData(e.gameName, e.gameType, e.mapType, e.version)}></img>)
-                    }
+                    <div>
+                      <span className={styles.winning_prize}>MAP</span>
+                      <span
+                        className={styles.tvm_font}
+                        style={{color: 'rgba(255, 122, 0, 1)'}}
+                      >
+                        {mapType}
+                      </span>
+                    </div>
                   </div>
 
-                  {/* <div className={styles.slidebar}>
+                  <div className={styles.winnings}>
+                    <div className={styles.spot_line}>
+                      <span className={styles.bar_font}>
+                        {' '}
+                        Only 30 spots Left
+                      </span>
+                      <span className={styles.bar_font}>20/50</span>
+                    </div>
+                    <Button className={styles.join_button}> Join</Button>
+                  </div>
+
+                  <div className={styles.winnings}>
+                    <div className={styles.slidebar}>
+                      {allTournaments &&
+                        allTournaments.map((e: any) => (
+                          <img
+                            src="../assests/cards.svg"
+                            alt="slides"
+                            onClick={() =>
+                              updateMainData(
+                                e.gameName,
+                                e.gameType,
+                                e.mapType,
+                                e.version,
+                              )
+                            }
+                          ></img>
+                        ))}
+                    </div>
+
+                    {/* <div className={styles.slidebar}>
                     <div className={styles.carusol}>
                       <div className={styles.row && styles.small}>
                         <div className={styles.imagegroup} style={{ animationDelay: '1s' }}>
@@ -157,93 +205,117 @@ function Tournament() {
                       </div>
                     </div>
                   </div> */}
-
-
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className={styles.container2}>
-            <img src='../assests/registeredmatches.svg' alt='slides'></img>
-            {/* <div className={styles.image_text}>
+            <div className={styles.container2}>
+              <img src="../assests/registeredmatches.svg" alt="slides"></img>
+              {/* <div className={styles.image_text}>
               <span>30/100</span>
               <div>
                 <span>BGMI Squad Match</span>
                 <span className={styles.view_more} style={{ color: 'rgba(255, 122, 0, 1)' }}>view more</span>
               </div>
             </div> */}
-            <div className={styles.Tournaments}>
-              <div className={styles.tournament_slider}>
-                <div className={styles.winning_prize}>
-                  <span> TYPE</span>
-                  <span className={styles.tvm_font} style={{ color: 'rgba(255, 214, 0, 1)' }}> Tournaments</span>
+              <div className={styles.Tournaments}>
+                <div className={styles.tournament_slider}>
+                  <div className={styles.winning_prize}>
+                    <span> TYPE</span>
+                    <span
+                      className={styles.tvm_font}
+                      style={{color: 'rgba(255, 214, 0, 1)'}}
+                    >
+                      {' '}
+                      Tournaments
+                    </span>
+                  </div>
+                  <div className={styles.winning_prize}>
+                    <span>Version</span>
+                    <span
+                      className={styles.tvm_font}
+                      style={{color: 'rgba(255, 214, 0, 1)'}}
+                    >
+                      TPP
+                    </span>
+                  </div>
+                  <div className={styles.winning_prize}>
+                    <span>MAP</span>
+                    <span
+                      className={styles.tvm_font}
+                      style={{color: 'rgba(255, 122, 0, 1)'}}
+                    >
+                      Erangel
+                    </span>
+                  </div>
                 </div>
-                <div className={styles.winning_prize}>
-                  <span>Version</span>
-                  <span className={styles.tvm_font} style={{ color: 'rgba(255, 214, 0, 1)' }}>TPP</span>
+                <div className={styles.room_create}>
+                  <div className={styles.winning_prize}>
+                    <span> Match start within </span>
+                    <span>10 minutes</span>
+                  </div>
+                  <div className={styles.winning_prize}>
+                    <span>0</span>
+                    <span>Minutes</span>
+                  </div>
                 </div>
-                <div className={styles.winning_prize}>
-                  <span>MAP</span>
-                  <span className={styles.tvm_font} style={{ color: 'rgba(255, 122, 0, 1)' }}>Erangel</span>
-                </div>
-              </div>
-              <div className={styles.room_create}>
-                <div className={styles.winning_prize}>
-                  <span> Match start within </span>
-                  <span>10 minutes</span>
-                </div>
-                <div className={styles.winning_prize}>
-                  <span>0</span>
-                  <span>Minutes</span>
-                </div>
-
-
-
-              </div>
-              <div className={styles.id_password}>
-                <span>Room Id: 5263487</span>
-                <span>Room password: 263</span>
-              </div>
-            </div>
-            <img src='../assests/registeredmatches.svg' alt='slides'></img>
-
-            <div className={styles.Tournaments}>
-              <div className={styles.tournament_slider}>
-                <div className={styles.winning_prize}>
-                  <span> TYPE</span>
-                  <span className={styles.tvm_font} style={{ color: 'rgba(255, 214, 0, 1)' }}> Tournaments</span>
-                </div>
-                <div className={styles.winning_prize}>
-                  <span>Version</span>
-                  <span className={styles.tvm_font} style={{ color: 'rgba(255, 214, 0, 1)' }}>TPP</span>
-                </div>
-                <div className={styles.winning_prize}>
-                  <span>MAP</span>
-                  <span className={styles.tvm_font} style={{ color: 'rgba(255, 122, 0, 1)' }}>Erangel</span>
+                <div className={styles.id_password}>
+                  <span>Room Id: 5263487</span>
+                  <span>Room password: 263</span>
                 </div>
               </div>
-              <div className={styles.room_create}>
-                <div className={styles.winning_prize}>
-                  <span> Match start within </span>
-                  <span>10 minutes</span>
+              <img src="../assests/registeredmatches.svg" alt="slides"></img>
+
+              <div className={styles.Tournaments}>
+                <div className={styles.tournament_slider}>
+                  <div className={styles.winning_prize}>
+                    <span> TYPE</span>
+                    <span
+                      className={styles.tvm_font}
+                      style={{color: 'rgba(255, 214, 0, 1)'}}
+                    >
+                      {' '}
+                      Tournaments
+                    </span>
+                  </div>
+                  <div className={styles.winning_prize}>
+                    <span>Version</span>
+                    <span
+                      className={styles.tvm_font}
+                      style={{color: 'rgba(255, 214, 0, 1)'}}
+                    >
+                      TPP
+                    </span>
+                  </div>
+                  <div className={styles.winning_prize}>
+                    <span>MAP</span>
+                    <span
+                      className={styles.tvm_font}
+                      style={{color: 'rgba(255, 122, 0, 1)'}}
+                    >
+                      Erangel
+                    </span>
+                  </div>
                 </div>
-                <div className={styles.winning_prize}>
-                  <span>0</span>
-                  <span>Minutes</span>
+                <div className={styles.room_create}>
+                  <div className={styles.winning_prize}>
+                    <span> Match start within </span>
+                    <span>10 minutes</span>
+                  </div>
+                  <div className={styles.winning_prize}>
+                    <span>0</span>
+                    <span>Minutes</span>
+                  </div>
                 </div>
-
-
-
-              </div>
-              <div className={styles.id_password}>
-                <span>Room Id: 5263487</span>
-                <span>Room password: 263</span>
+                <div className={styles.id_password}>
+                  <span>Room Id: 5263487</span>
+                  <span>Room password: 263</span>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
