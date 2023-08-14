f88064b36a7494bcb57c7c622c05083e31d2df19
Diksha sharma <diksha.sharma@technogetic.com>
2023-08-14 17:00:35 +0530

    delete api changes and update api

--- src/app/spectatorDashboard/Deletespec/page.tsx
+++ src/app/spectatorDashboard/Deletespec/page.tsx
@@ -1,46 +1,50 @@
 'use client';
-// import React, {useState} from 'react';
-// import sendRequest from '../../../services/api/apiServices';
+import React, {useState} from 'react';
+import sendRequest from '../../../services/api/apiServices';
+import {AiOutlineDelete} from 'react-icons/ai';
+import styles from '../../../styles/Spectator.module.scss';
 
-// interface DeletespecProps {
-//   deletedata: string;
-//   Id: string;
-// }
+const Deletespec = ({Id}: any) => {
+  const [deletModal, setDeleteModal] = useState(false);
 
-// const Deletespec: React.FC<DeletespecProps> = ({deletedata: method, Id}) => {
-//   const [deletModal, setDeleteModal] = useState(false);
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-//   const [dataDeleted, setDataDeleted] = useState(false);
-//   const handleDelete = async () => {
-//     const token = localStorage.getItem('jwtToken');
-//     const deleteResponse = await sendRequest(`room/rooms/${Id}`, {
-//       method,
-//       headers: {Authorization: `Bearer ${token}`},
-//     });
-//     setDataDeleted(true);
-//   };
+  const handleDelete = async () => {
+    const token = localStorage.getItem('jwtToken');
+    const deleteResponse = await sendRequest(`room/rooms/${Id}`, {
+      method: 'DELETE',
+      headers: {Authorization: `Bearer ${token}`},
+    });
+  };
 
-//   return (
-//     <>
-//       {' '}
-//       <button onClick={() => setDeleteModal(true)}>delete</button>
-//       {deletModal ? (
-//         <div>
-//           <h4>Delete</h4>
-//           <p>Are you sure want to delete this room?</p>
-//           <button onClick={handleDelete}>Delete</button>
-//           <button>cancel</button>
-//         </div>
-//       ) : (
-//         ''
-//       )}
-//     </>
-//   );
-// };
-import React from 'react';
-
-const Deletespec = () => {
-  return <div>delete</div>;
+  return (
+    <>
+      <p onClick={() => setDeleteModal(true)}>
+        <AiOutlineDelete style={{color: '#ff7800', size: '18'}} />
+      </p>
+      {deletModal ? (
+        <div className={styles.main_del_sec}>
+          <div className={styles.inner_del_sec}>
+            <h4 className={styles.del_title}>Delete</h4>
+            <p className={styles.sec_heading}>
+              Are you sure want to delete this room?
+            </p>
+            <div className={styles.del_btn_sec}>
+              <button className={styles.dele_btn} onClick={handleDelete}>
+                Delete
+              </button>
+              <button
+                className={styles.canc_btn}
+                onClick={() => setDeleteModal(false)}
+              >
+                cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ''
+      )}
+    </>
+  );
 };
 
 export default Deletespec;
