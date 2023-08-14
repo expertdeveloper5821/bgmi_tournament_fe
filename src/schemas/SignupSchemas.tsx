63e95c48349d1484b975d990580a4062be88822c
Diksha sharma <diksha.sharma@technogetic.com>
2023-08-14 20:34:45 +0530

    login validation changes

--- src/schemas/SignupSchemas.tsx
+++ src/schemas/SignupSchemas.tsx
@@ -9,7 +9,12 @@ export const SignupSchema = Yup.object().shape({
     .email('Invalid email')
     .required('Please enter your email')
     .matches(emailRegex, 'Invalid email'),
-  password: Yup.string().required('Please enter your password'),
+  password: Yup.string()
+    .required('Please enter your password')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+      'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character',
+    ),
 });
 
 export const loginSchema = Yup.object().shape({
