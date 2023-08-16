310acf4589b076e581ce5f8759b290b707bdd3bc
developerAkshay1 <akshay.kumar@technogetic.com>
2023-08-16 16:28:16 +0530

    sidebar fixes and added context provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
+"use client"
 import Loader from './auth/login/page';
 import RootLayout from './layout';
-import {ToastContainer} from 'react-toastify';
 export default function Home() {
   return (
     <RootLayout>
       <Loader />
-      <ToastContainer />
     </RootLayout>
   );
 }
