c9459237a117d92c1ad05af49b1aae76901be029
developerAkshay1 <akshay.kumar@technogetic.com>
2023-09-04 15:50:28 +0530

    recent changes

--- src/app/userDashboard/video/videoCard/videoCard.tsx
+++ src/app/userDashboard/video/videoCard/videoCard.tsx
@@ -1,9 +1,9 @@
 'use client';
-import React, {useEffect, useState} from 'react';''
+import React, {useEffect, useState} from 'react';
 import styles from '../../../../styles/videoCard.module.scss';
 import {sendRequest} from '@/services/auth/auth_All_Api';
 import Image from 'next/image';
-
+import {toast} from 'react-toastify';
 
 interface VideoInfo {
   date: string;
@@ -15,13 +15,13 @@ interface CustomPaginationProps {
 }
 
 const VideoCard: React.FC<CustomPaginationProps> = ({onDataUpdate}) => {
+
   const [data, setData] = useState<VideoInfo[]>([]);
-   // will use spectator login token here
-  console.log(data);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const accessToken = localStorage.getItem('jwttoken');        
+        const accessToken = localStorage.getItem('jwttoken');
         const response = await sendRequest('/role/allvideolink', {
           method: 'GET',
           headers: {
@@ -37,29 +37,66 @@ const VideoCard: React.FC<CustomPaginationProps> = ({onDataUpdate}) => {
     };
     fetchData();
   }, []);
+
+  // copy link to clipboard
+  const handleCopyClick = async (link: any) => {
+    try {
+      const tempInput = document.createElement('input');
+      tempInput.value = link;
+      document.body.appendChild(tempInput);
+      tempInput.select();
+      await navigator.clipboard.writeText(link);
+      document.body.removeChild(tempInput);
+      toast.success('link copied to clipboard');
+    } catch (error) {
+      console.log('errorr', error);
+    }
+  };
+
+ // Redirect to the link directly
+  const handleRedirectClick = (linkText:any) => {
+    window.location.href = linkText;
+  };
+
   return (
     <div>
-      { data && data.length > 0 &&
+      {data &&
+        data.length > 0 &&
         data.map((info: VideoInfo, index: number) => {
           return (
             <div className={styles.main_container} key={index}>
               <div className={styles.bannercontainer}>
-                <Image src="/assests/ytbanner.svg" alt='ytbanner' height={100} width={100} className={styles.ytbanner} />
+                <Image
+                  src="/assests/ytbanner.svg"
+                  alt="ytbanner"
+                  height={100}
+                  width={100}
+                  className={styles.ytbanner}
+                />
               </div>
               <div className={styles.gameInfo}>
                 <h1 className={styles.heading}>BGMI SQUAD MATCH</h1>
                 <h4 className={styles.time}>
-                  Time :{info.date} at {info.time}{' '}
+                  Time :{info.date} at {info.time}
                 </h4>
-                <h4 className={styles.time}>{info.videoLink}</h4>
+                {/* <p className={styles.time}>{info.videoLink}</p> */}
                 <div className={styles.button_maincontainer}>
-                  <div className={styles.btnContainer}>
+                  <div
+                    className={styles.btnContainer}
+                    onClick={() => handleCopyClick(info.videoLink)}
+                  >
                     <span className={styles.copyimg}>
-                      <Image src="/assests/copy.svg" alt="copy" height={100} width={100} />
+                      <Image
+                        src="/assests/copy.svg"
+                        alt="copy"
+                        height={100}
+                        width={100}
+                        className={styles.copyicon}
+                      />
                     </span>
                     <button className={styles.btn}> Copy link</button>
                   </div>
-                  <div className={styles.btnContainer}>
+                  <div className={styles.btnContainer} onClick={()=>handleRedirectClick(info.videoLink)}>
                     <button className={styles.btn}> Watch video</button>
                   </div>
                 </div>
