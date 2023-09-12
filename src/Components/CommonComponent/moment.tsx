a0a57dbb5c40b741127f2221f66d27afb8bbd9b0
shivangi-gupta123 <124764991+shivangi-gupta123@users.noreply.github.com>
2023-09-12 15:53:23 +0530

    room module

---
+++ src/Components/CommonComponent/moment.tsx
@@ -0,0 +1,14 @@
+import moment from 'moment';
+// If you installed moment-timezone
+
+// ...
+
+export const formatDate = ({ date, format = 'l' }: { date: Date | string, format?: string }): string => {
+
+    // Format the date as you desire
+    return moment(date).format(format);
+};
+
+export const formatTime = ({ time, format = 'LT' }: { time: Date | string, format?: string }): string => {
+    return moment(time).format(format)
+};
--- src/app/adminDashboard/room/page.tsx
+++ src/app/adminDashboard/room/page.tsx
@@ -1,80 +1,133 @@
 'use client';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../../../styles/Dashboard.module.scss';
-import {Navbar} from '../../../Components/Navbar/Navbar';
-import TableData, {StudentProfile} from '../../../Components/Table/Table';
+import { Navbar } from '../../../Components/Navbar/Navbar';
+
+import TableData, { RoomData } from '../../../Components/Table/Table'
 import assignmentData from '../../../utils/CreateAssignment.json';
 //@ts-ignore
-import {Pagination} from 'technogetic-iron-smart-ui';
-import {BtnDashboard} from '../../../Components/CommonComponent/BtnDashboard';
+import { Pagination } from 'technogetic-iron-smart-ui';
+import { BtnDashboard } from '../../../Components/CommonComponent/BtnDashboard';
 import RequireAuthentication from '../../../utils/requireAuthentication';
-import {sendRequest} from '@/services/auth/auth_All_Api';
+// import apiServices from '@/services/api/apiServices';
+import { sendRequest } from '@/services/auth/auth_All_Api';
+import { toast } from 'react-toastify';
+import Loader from '@/Components/Loader/Loader';
+import router from 'next/router';
 
-export interface IAppProps {}
+
+export interface IAppProps { }
 
 function page() {
   const [currentPage, setCurrentPage] = useState(1);
-  const [paginatedData, setPaginatedData] = useState<StudentProfile[]>([]);
+  const [paginatedData, setPaginatedData] = useState<RoomData[]>([]);
   const rowPerPage = 8;
-
-  const transformedStudentData = assignmentData.studentData.map(
-    (item: StudentProfile) => ({
-      StudentName: item.StudentName,
-      Student: item.Student,
-      studentID: item.studentID,
-      Mobile: item.Mobile,
-      Course: item.Course,
-    }),
-  );
+  const [roomData, setRoomData] = useState<RoomData[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const imageIcon: string = 'room';
 
   useEffect(() => {
-    const startIndex = (currentPage - 1) * rowPerPage;
-    const endIndex = startIndex + rowPerPage;
-    const paginatedData = transformedStudentData.slice(startIndex, endIndex);
-    setPaginatedData(paginatedData);
-  }, [currentPage, transformedStudentData]);
+    // Simulate data loading or any async operation
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
 
-  const onPageChange = (page: number) => {
-    setCurrentPage(page);
+  const getAllTournaments = async () => {
+    const tokens = localStorage.getItem('jwtToken');
+    const tournamentResponse = await sendRequest('/room/rooms', {
+      method: 'GET',
+      headers: { 'Authorization': `Bearer ${tokens}` }
+    });
+
+    setRoomData(tournamentResponse.data);
+
+  }
+
+  useEffect(() => {
+    getAllTournaments();
+  }, [])
+
+  const deleteroomId = async (_id: any) => {
+    console.log("id__________", _id)
+    setIsLoading(true);
+    try {
+      const tokens = localStorage.getItem('jwtToken');
+      console.log(tokens)
+      const response = await sendRequest(`/room/rooms/${_id}`, {
+        method: 'delete',
+        headers: { 'Authorization': `Bearer ${tokens}` }
+      });
+      // const updatedData = roomData.filter(data => data.roomId !== _id);
+      // setRoomData(updatedData);
+      getAllTournaments();
+      if (response) {
+
+        const success = response.data.message;
+        toast.success(success);
+
+      }
+
+    } catch (error) {
+      console.error('Error deleting room:', error);
+
+    } finally {
+      setIsLoading(false);
+    }
   };
 
+
   const columns: string[] = [
-    'roomId',
-    'password',
-    'gameName',
-    'gameType',
-    'mapType',
-    'createdBy',
-    'createdAt',
+    'Created By',
+    'Room Id',
+    'Password',
+    'Game Name',
+    'Game Type',
+    'Map Type',
+    // 'MapImg',
+    'Version',
+    'Time',
+    'Date',
+    // 'Last Servival',
+    // 'Highest Kill',
+    // 'Second Win',
+    // 'Third Win',
+
+
   ];
+
 
   return (
     <>
       <RequireAuthentication>
+
         <div className={styles.main_container}>
           <div className={styles.abcd}>
             <div className={styles.sidebar_wrapper}>
               <Navbar />
-              <h1>Welcome to Admin Dashboard</h1>
-              <BtnDashboard />
-              <TableData
-                studentData={paginatedData}
-                columns={columns}
-                showAdditionalButton={true}
-              />
-              {/* <Pagination
-                currentPage={currentPage}
-                totalPages={Math.ceil(
-                  transformedStudentData.length / rowPerPage,
-                )}
-                onPageChange={onPageChange}
-              /> */}
+              <h1 className={styles.heading}>Welcome to Admin Dashboard</h1>
+              {/* <BtnDashboard /> */}
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <TableData
+                  imageIcon={imageIcon}
+                  roomData={roomData}
+                  columns={columns}
+                  showAdditionalButton={true} userData={[]} teamData={[]} spectatorData={[]} deleteroomId={deleteroomId} setSetSpectatorId={undefined} setModal={undefined} updateSpectatorByid={undefined} />
+              )}
+
             </div>
           </div>
         </div>
+
       </RequireAuthentication>
     </>
   );
 }
 
+
+
 export default page;
+
+
