fdd01e0c58ee9c98e601e09e04e41e7a0ab25ac0
vijay gupta <vijay.gupta@technogetic.com>
2023-09-18 15:12:29 +0530

    build error add in preCommit hook

--- src/utils/globalfunctions.ts
+++ src/utils/globalfunctions.ts
@@ -1,8 +1,18 @@
 import jwt_decode from 'jwt-decode';
-export const decodeJWt = (token: string): void => {
+
+const decodeJWt = (token: string): void => {
   if (token) {
     return jwt_decode(token);
   } else {
     return;
   }
 };
+
+const getTokenFromLS = () => {
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('jwtToken');
+    return token;
+  }
+};
+
+export { decodeJWt, getTokenFromLS };
