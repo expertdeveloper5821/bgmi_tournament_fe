df055686d1c2770bfd7e087d20945c3c833b611c
Diksha sharma <diksha.sharma@technogetic.com>
2023-09-19 22:18:24 +0530

    single register match code changes

--- src/app/userDashboard/tournament/CountdownComponent.tsx
+++ src/app/userDashboard/tournament/CountdownComponent.tsx
@@ -1,22 +1,4 @@
 'use client';
-// import React from 'react';
-// import styles from '../../../styles/CountdownComponent.module.scss';
-
-// function CountdownComponent(props: {
-//   roomidd: string;
-//   password: string;
-//   date: string;
-//   time: string;
-// }) {
-//   return (
-//     <div className={styles.id_password}>
-//       <span>Room Id: {props.roomidd}</span>
-//       <span>Room password: {props.password}</span>
-//     </div>
-//   );
-// }
-
-// export default CountdownComponent;
 const CountdownComponent = (dateAndTime: string, roomUuid: string, setVisibleRooms: React.Dispatch<React.SetStateAction<string[]>>) => {
   if (dateAndTime && roomUuid) {
     setInterval(() => {
