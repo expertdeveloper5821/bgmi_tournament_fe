78e3e9fbf24c45cbc8e97bc5bb0a39fa89b32e36
shivangi-gupta123 <124764991+shivangi-gupta123@users.noreply.github.com>
2023-10-17 15:16:53 +0530

    code

--- src/app/userDashboard/videos/page.tsx
+++ src/app/userDashboard/videos/page.tsx
@@ -4,10 +4,75 @@ import Loading from '../loading';
 import styles from '@/styles/Dashboard.module.scss';
 import { Navbar } from '@/Components/CommonComponent/Navbar/Navbar';
 import CustomPagination from '@/Components/CommonComponent/Pagination/Pagination';
+import { sendRequest } from '@/utils/axiosInstanse';
 import Image from 'next/image';
+
+interface VideoInfo {
+    _id: string;
+    title: string;
+    dateAndTime: string;
+    videoLink: string;
+}
+
+function formatDateTime(dateTime: string) {
+    const dateObj = new Date(dateTime);
+    const month = String(dateObj.getMonth() + 1).padStart(2, '0');
+    const day = String(dateObj.getDate()).padStart(2, '0');
+    const year = dateObj.getFullYear();
+    const hours = dateObj.getHours();
+    const minutes = dateObj.getMinutes();
+    const amOrPm = hours >= 12 ? 'pm' : 'am';
+
+    return `Time: ${month}/${day}/${year} at ${hours % 12}:${minutes.toString().padStart(2, '0')} ${amOrPm}`;
+}
+
+interface CustomPaginationProps {
+    onDataUpdate: (data: any) => void;
+}
 
 const Page: React.FC = () => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [data, setData] = useState<VideoInfo[]>([]);
+    console.log("data---->", data)
+
+    const [currentCardIndex, setCurrentCardIndex] = useState(0);
+
+    // Function to go to the previous card
+    const goToPreviousCard = () => {
+        if (currentCardIndex > 0) {
+            setCurrentCardIndex(currentCardIndex - 1);
+        }
+    };
+
+    // Function to go to the next card
+    const goToNextCard = () => {
+        if (currentCardIndex < data.length - 1) {
+            setCurrentCardIndex(currentCardIndex + 1);
+        }
+    };
+
+
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const accessToken = localStorage.getItem('jwttoken');
+                const response = await sendRequest('/role/allvideolink', {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                });
+                const responseData = response?.data?.data as VideoInfo[];
+                setData(responseData);
+                console.log("response===>", response)
+            } catch (error) {
+                // console.log(error);
+            }
+        };
+        fetchData();
+    }, []);
 
     return (
         <>
@@ -40,66 +105,42 @@ const Page: React.FC = () => {
                                         <div className={styles.col}>
                                             <h1 className={styles.head}>BGMI SQUAD MATCH</h1>
                                             <div className={styles.flex}>
-                                                <span>Time: 02/08/2023 at 06:00 pm</span>
+
+                                                <span>Time</span>
+
                                                 <img src="../assests/copylink.svg" alt="videoimage" className={styles.copy_link} />
+
+                                                {/* <img src="../assests/copylink.svg" alt="videoimage" className={styles.copy_link} /> */}
                                             </div>
                                             <div className={styles.flex_end}>
-                                                <img src="../assests/arrow buttonnext.svg" alt="nextbutton" className={styles.button} />
-                                                <img src="../assests/arrow buttonprevious.svg" alt="previousbutton" className={styles.button} />
+                                                <img src="../assests/arrow buttonnext.svg" alt="nextbutton" className={styles.button} onClick={goToPreviousCard} />
+                                                <img src="../assests/arrow buttonprevious.svg" alt="previousbutton" className={styles.button} onClick={goToNextCard} />
                                             </div>
 
                                         </div>
 
                                         <div className={styles.card}>
-                                            <div className={styles.card1}>
-                                                <img src="../assests/cardimage1.svg" alt="cardImage" className={styles.cardImage} />
-                                                <div className={styles.flex_absolute}>
-                                                    <img src="../assests/playicon.svg" alt="cardImage" className={styles.playicon} />
-                                                    <span className={styles.time}>8:00</span>
-                                                </div>
-                                                <div className={styles.flex_justify}>
-                                                    <span className={styles.match_name}>BGMI Squad Match</span>
-                                                    <img src="../assests/copy.svg" alt="videoimage" className={styles.copy_link} /></div>
-                                                <span className={styles.timedate}>Time: 02/08/2023 at 06:00 pm</span>
-                                            </div>
-                                            <div className={styles.card1}>
-                                                <img src="../assests/cardImge2.svg" alt="cardImage" className={styles.cardImage} />
-                                                <div className={styles.flex_absolute}>
-                                                    <img src="../assests/playicon.svg" alt="cardImage" className={styles.playicon} />
-                                                    <span className={styles.time}>8:00</span>
-                                                </div>
-                                                <div className={styles.flex_justify}>
-                                                    <span className={styles.match_name}>BGMI Squad Match</span>
-                                                    <img src="../assests/copy.svg" alt="videoimage" className={styles.copy_link} /></div>
-                                                <span className={styles.timedate}>Time: 02/08/2023 at 06:00 pm</span>
-                                            </div>
-                                            <div className={styles.card1}>
-                                                <img src="../assests/cardimage1.svg" alt="cardImage" className={styles.cardImage} />
-                                                <div className={styles.flex_absolute}>
-                                                    <img src="../assests/playicon.svg" alt="cardImage" className={styles.playicon} />
-                                                    <span className={styles.time}>8:00</span>
-                                                </div>
-                                                <div className={styles.flex_justify}>
-                                                    <span className={styles.match_name}>BGMI Squad Match</span>
-                                                    <img src="../assests/copy.svg" alt="videoimage" className={styles.copy_link} /></div>
-                                                <span className={styles.timedate}>Time: 02/08/2023 at 06:00 pm</span>
-                                            </div>
-
-                                            <div className={styles.card1}>
-                                                <img src="../assests/card4.svg" alt="cardImage" className={styles.cardImage} />
-                                                <div className={styles.flex_absolute}>
-                                                    <img src="../assests/playicon.svg" alt="cardImage" className={styles.playicon} />
-                                                    <span className={styles.time}>8:00</span>
-                                                </div>
-                                                <div className={styles.flex_justify}>
-                                                    <span className={styles.match_name}>BGMI Squad Match</span>
-                                                    <img src="../assests/copy.svg" alt="videoimage" className={styles.copy_link} /></div>
-                                                <span className={styles.timedate}>Time: 02/08/2023 at 06:00 pm</span>
-                                            </div>
+                                            {data.map((item) => (
+                                                <>
+                                                    <div className={styles.card1}>
+                                                        <img src="../assests/cardimage1.svg" alt="cardImage" className={styles.cardImage} />
+                                                        <div className={styles.flex_absolute}>
+                                                            <img src="../assests/playicon.svg" alt="cardImage" className={styles.playicon} />
+                                                            <span className={styles.time}>8:00</span>
+                                                        </div>
+                                                        <div className={styles.flex_justify}>
+                                                            <span className={styles.match_name}>{item.title}</span>
+                                                            <a href={item.videoLink} target="_blank" rel="noopener noreferrer">
+                                                                <img src="../assests/copy.svg" alt="videoimage" className={styles.copy_link} /> </a></div>
+                                                        <span className={styles.timedate}>{formatDateTime(item.dateAndTime)}</span>
+                                                    </div>
+                                                </>
+                                            ))}
                                         </div>
 
                                     </div>
                                 </div>
+
                             </div>
 
                         </div>
