e508832c588900486ef4fff2119f83f1b75d1c46
devashish <devashish.sharma@technogetic.com>
2023-10-26 15:55:11 +0530

    refactored ForgetPassword form with Formik

--- src/Components/Forms/AuthForms/ForgetPasswordForm.tsx
+++ src/Components/Forms/AuthForms/ForgetPasswordForm.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, ChangeEvent } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import styles from '@/styles/auth.module.scss';
 import { useRouter } from 'next/navigation';
@@ -7,115 +7,50 @@ import { useRouter } from 'next/navigation';
 import { Button, Input } from 'technogetic-iron-smart-ui';
 import Image from 'next/image';
 import { sendRequest } from '@/utils/axiosInstanse';
-import { useFormik } from 'formik';
+import { FormikHelpers, useFormik } from 'formik';
 import { forgetPasswordSchema } from '@/utils/schema';
+import { toast } from 'react-toastify';
+import { ForgetFormValues } from '@/types/formsTypes';
 
-interface ResetPasswordProps {}
+const initialValues: ForgetFormValues = {
+  email: '',
+};
 
 export function ForgetPasswordForm(): JSX.Element {
-  const [email, setEmail] = useState<string>('');
-  const [isEmailValid, setIsEmailValid] = useState<boolean>(true);
-
   const router = useRouter();
 
-  const initialValues: any = {
-    email: '',
-  };
+  const { values, touched, errors, handleSubmit, handleChange, handleBlur, isSubmitting } =
+    useFormik({
+      initialValues,
+      validationSchema: forgetPasswordSchema,
+      onSubmit: async (
+        values: ForgetFormValues,
+        { setSubmitting }: FormikHelpers<ForgetFormValues>,
+      ) => {
+        setSubmitting(true);
 
+        const { email } = values;
 
-  // const { values, touched, errors, handleSubmit, handleChange, handleBlur, setFieldValue } =
-  //   useFormik({
-  //     initialValues,
-  //     validationSchema: forgetPasswordSchema,
-  //     onSubmit: async (values: any, { setSubmitting }: any) => {
-  //       setIsLoading(true);
-  //       const { email, password } = values;
+        try {
+          const response: any = await sendRequest('user/forget-password', {
+            method: 'POST',
+            data: { email },
+          });
 
-  //       try {
-  //         const response: any = await sendRequest('user/login', {
-  //           method: 'POST',
-  //           data: { email, password },
-  //         });
-
-  //         setIsLoading(false);
-  //         const decodedToken: any = decodeJWt(response?.data?.userData?.token);
-
-  //         if (response.status === 200) {
-  //           // Below need to figure out why we want this.
-
-  //           // const userDetails = {
-  //           //   name: decodedToken?.fullName,
-  //           //   email: decodedToken?.email,
-  //           // };
-
-  //           // updateUserInfo(userDetails);
-  //           // updateToken(decodedToken);
-
-  //           const date = new Date();
-  //           const expirationTime = date.setHours(date.getHours() + 1);
-  //           // Below line is for testing Purpose only.
-  //           // const expirationTime =  date.setMinutes(date.getMinutes() + 1);
-
-  //           console.log(
-  //             'resonseeeeee 0==>',
-  //             response,
-  //             'decodedToken',
-  //             decodedToken,
-  //             'expirationTime',
-  //             expirationTime,
-  //           );
-
-  //           if (rememberMe) {
-  //             localStorage.setItem('rememberMe', rememberMe.toString());
-  //             localStorage.setItem('email', email);
-  //           } else {
-  //             localStorage.setItem('rememberMe', rememberMe.toString());
-  //             localStorage.removeItem('email');
-  //           }
-  //           localStorage.setItem('jwtToken', response?.data?.userData?.token);
-  //           localStorage.setItem('expirationTime', expirationTime.toString());
-  //           toast.success(response?.data?.message);
-  //           handleRedirect(response?.data?.userData?.token);
-  //         } else {
-  //           setError('Invalid email or password');
-  //         }
-  //       } catch (error: any) {
-  //         console.log("inside catch error", error);
-  //         setIsLoading(false);
-  //         toast.error(error?.message);
-  //       } finally {
-  //         setSubmitting(false);
-  //       }
-  //     },
-  //   });
-
-
-  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-    const isValid = validateEmail(event.target.value);
-    setIsEmailValid(isValid);
-  };
-
-  const sendEmail = async () => {
-    try {
-      const response = await sendRequest('user/forget-password', {
-        method: 'POST',
-        data: { email },
-      });
-      router.push('/auth/mailpage');
-    } catch (error) {
-      console.error('Password recovery error:', error);
-    }
-  };
-
-  const validateEmail = (email: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-
+          if (response.status === 200) {
+            toast.success(response.data.message);
+            router.push('/auth/mailpage');
+          }
+          setSubmitting(false);
+        } catch (error) {
+          setSubmitting(false);
+          toast.error(error.message);
+        }
+      },
+    });
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className={styles.input_box}>
         <label className={styles.email} htmlFor="email">
           <Image src="../assests/fullnameicon.svg" alt="fullname" width={30} height={20} />
@@ -123,22 +58,24 @@ export function ForgetPasswordForm(): JSX.Element {
         <Input
           type="email"
           id="email"
-          className={`${styles.email_wrapper} ${isEmailValid ? '' : styles.invalid}`}
+          className={`${styles.email_wrapper} ${!errors.email ? '' : styles.invalid}`}
           placeholder="Enter Email"
-          value={email}
-          onChange={handleEmailChange}
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
       </div>
-      {!isEmailValid && <p className={styles.error}>Please enter a valid email</p>}
+      {errors.email && touched.email && <div className={styles.error}>{errors.email}</div>}
+
       <div className={styles.button_wrapper}>
-        <Button variant="contained" className={styles.SignIn_button} onClick={sendEmail}>
-          Recover Password
+        <Button
+          disabled={isSubmitting}
+          variant="contained"
+          className={styles.SignIn_button}
+          onClick={handleSubmit}
+        >
+          {isSubmitting ? 'Loading...' : 'Recover Password'}
         </Button>
-      </div>
-      <div className={styles.signin}>
-        <span>
-          <Link href="/">Remember it?&nbsp;Sign in here</Link>
-        </span>
       </div>
     </form>
   );
