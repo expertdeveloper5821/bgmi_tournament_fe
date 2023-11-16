aa0c6fcec3d5ffa8b7aff7517d1363fa4261bd99
devashish <devashish.sharma@technogetic.com>
2023-11-16 12:13:21 +0530

    Navbar added for video and teams in admindashboard also temperarely centered

--- src/app/adminDashboard/teams/page.tsx
+++ src/app/adminDashboard/teams/page.tsx
@@ -1,11 +1,13 @@
 'use client';
+import { Navbar } from '@/Components/CommonComponent/Navbar/Navbar';
 import IsAuthenticatedHoc from '@/Components/HOC/IsAuthenticatedHoc';
 import React from 'react';
 
 const page = () => {
   return (
     <IsAuthenticatedHoc>
-      <div>
+      <Navbar />
+      <div style={{ textAlign: 'center' }}>
         <h2>Teams</h2>
       </div>
     </IsAuthenticatedHoc>
