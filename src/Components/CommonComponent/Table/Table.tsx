c4ebed60adad8ac0be8ec67e5e9c118ab8eb7eb7
devashish <devashish.sharma@technogetic.com>
2023-11-30 17:41:21 +0530

    No data found and its css adjusted

--- src/Components/CommonComponent/Table/Table.tsx
+++ src/Components/CommonComponent/Table/Table.tsx
@@ -76,96 +76,100 @@ const TableData = ({ data, columns, deleteroom, type, handleEdit }: TablePropsTy
 
   return (
     <>
-      <Table className={styles.table_content}>
-        <TableHeader className={styles.tableHeader}>
-          <TableRow className={styles.tableRow}>
-            {columns?.map((columnName) => (
-              <TableHead className={styles.table_head} key={columnName}>
-                <div className={styles.filter}>
-                  {columnName}
-                  {filterKeys.includes(columnName) && (
-                    <div>
-                      <FaLongArrowAltUp
-                        style={{ color: `${activeFilter === 1 ? '#FF7A00' : ''}` }}
-                        onClick={() => {
-                          setactiveFilter(1);
-                          handleSort(columnName, 'upArrow');
-                        }}
-                      />
-                      <FaLongArrowAltDown
-                        style={{ color: `${activeFilter === 2 ? '#FF7A00' : ''}` }}
-                        onClick={() => {
-                          setactiveFilter(2);
-                          handleSort(columnName, 'downArrow');
-                        }}
-                      />
-                    </div>
-                  )}
-                </div>
-              </TableHead>
-            ))}
-            <TableHead className={styles.table_head}>
-              <div className={styles.filter}>Actions</div>
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-
-        <TableBody className={styles.table_body}>
-          {sortedData?.slice(startIndex, endIndex)?.map((data: TableDataType, index: number) => {
-            if (type === 'ROOMS') {
-              return (
-                <TableRow className={styles.table_rowdata} key={index}>
-                  <TableCell className={styles.table_cell}>{data?.createdBy}</TableCell>
-                  <TableCell className={styles.table_cell}>{data?.roomId}</TableCell>
-                  <TableCell className={styles.table_cell}>{data?.password}</TableCell>
-                  <TableCell className={styles.table_cell}>{data?.gameName}</TableCell>
-                  <TableCell className={styles.table_cell}>{data?.gameType}</TableCell>
-                  <TableCell className={styles.table_cell}>{data?.mapType}</TableCell>
-                  <TableCell className={styles.table_cell}>{data?.version}</TableCell>
-                  <TableCell className={styles.table_cell}>
-                    {getFormattedDateOrTime(data?.dateAndTime, 'Time')!}
-                  </TableCell>
-                  <TableCell className={styles.table_cell}>
-                    {getFormattedDateOrTime(data?.dateAndTime, 'Date')!}
-                  </TableCell>
-                  <TableCell className={`${styles.table_cell} ${styles.action_td}`}>
-                    <RiDeleteBin6Line
-                      className={styles.del}
-                      onClick={() => deleteroom && deleteroom(data._id)}
-                    />
-                  </TableCell>
-                </TableRow>
-              );
-            } else if (type === 'SPECTATOR' || type === 'USERS') {
-              return (
-                <TableRow className={styles.table_rowdata} key={index}>
-                  <TableCell className={styles.table_cell}>{data?.fullName}</TableCell>
-                  <TableCell className={styles.table_cell}>{data?.userName || '--'}</TableCell>
-                  <TableCell className={styles.table_cell}>{data?.email}</TableCell>
-                  <TableCell className={`${styles.table_cell} ${styles.action_td}`}>
-                    {type === 'SPECTATOR' && (
-                      <MdEdit
-                        className={styles.del}
-                        onClick={() => handleEdit && handleEdit(data)}
-                      />
+      {sortedData.length ? (
+        <Table className={styles.table_content}>
+          <TableHeader className={styles.tableHeader}>
+            <TableRow className={styles.tableRow}>
+              {columns?.map((columnName) => (
+                <TableHead className={styles.table_head} key={columnName}>
+                  <div className={styles.filter}>
+                    {columnName}
+                    {filterKeys.includes(columnName) && (
+                      <div>
+                        <FaLongArrowAltUp
+                          style={{ color: `${activeFilter === 1 ? '#FF7A00' : ''}` }}
+                          onClick={() => {
+                            setactiveFilter(1);
+                            handleSort(columnName, 'upArrow');
+                          }}
+                        />
+                        <FaLongArrowAltDown
+                          style={{ color: `${activeFilter === 2 ? '#FF7A00' : ''}` }}
+                          onClick={() => {
+                            setactiveFilter(2);
+                            handleSort(columnName, 'downArrow');
+                          }}
+                        />
+                      </div>
                     )}
-                    <RiDeleteBin6Line
-                      className={styles.del}
-                      onClick={() => deleteroom && deleteroom(data.userUuid)}
-                    />
-                  </TableCell>
-                </TableRow>
-              );
-            }
-          })}
-        </TableBody>
-        <Pagination
-          totalItems={totalItems}
-          itemsPerPage={itemsPerPage}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-        />
-      </Table>
+                  </div>
+                </TableHead>
+              ))}
+              <TableHead className={styles.table_head}>
+                <div className={styles.filter}>Actions</div>
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody className={styles.table_body}>
+            {sortedData.slice(startIndex, endIndex)?.map((data: TableDataType, index: number) => {
+              if (type === 'ROOMS') {
+                return (
+                  <TableRow className={styles.table_rowdata} key={index}>
+                    <TableCell className={styles.table_cell}>{data?.createdBy}</TableCell>
+                    <TableCell className={styles.table_cell}>{data?.roomId}</TableCell>
+                    <TableCell className={styles.table_cell}>{data?.password}</TableCell>
+                    <TableCell className={styles.table_cell}>{data?.gameName}</TableCell>
+                    <TableCell className={styles.table_cell}>{data?.gameType}</TableCell>
+                    <TableCell className={styles.table_cell}>{data?.mapType}</TableCell>
+                    <TableCell className={styles.table_cell}>{data?.version}</TableCell>
+                    <TableCell className={styles.table_cell}>
+                      {getFormattedDateOrTime(data?.dateAndTime, 'Time')!}
+                    </TableCell>
+                    <TableCell className={styles.table_cell}>
+                      {getFormattedDateOrTime(data?.dateAndTime, 'Date')!}
+                    </TableCell>
+                    <TableCell className={`${styles.table_cell} ${styles.action_td}`}>
+                      <RiDeleteBin6Line
+                        className={styles.del}
+                        onClick={() => deleteroom && deleteroom(data._id)}
+                      />
+                    </TableCell>
+                  </TableRow>
+                );
+              } else if (type === 'SPECTATOR' || type === 'USERS') {
+                return (
+                  <TableRow className={styles.table_rowdata} key={index}>
+                    <TableCell className={styles.table_cell}>{data?.fullName}</TableCell>
+                    <TableCell className={styles.table_cell}>{data?.userName || '--'}</TableCell>
+                    <TableCell className={styles.table_cell}>{data?.email}</TableCell>
+                    <TableCell className={`${styles.table_cell} ${styles.action_td}`}>
+                      {type === 'SPECTATOR' && (
+                        <MdEdit
+                          className={styles.del}
+                          onClick={() => handleEdit && handleEdit(data)}
+                        />
+                      )}
+                      <RiDeleteBin6Line
+                        className={styles.del}
+                        onClick={() => deleteroom && deleteroom(data.userUuid)}
+                      />
+                    </TableCell>
+                  </TableRow>
+                );
+              }
+            })}
+          </TableBody>
+          <Pagination
+            totalItems={totalItems}
+            itemsPerPage={itemsPerPage}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          />
+        </Table>
+      ) : (
+        <h1 className={styles.no_data_found_heading}>No data found</h1>
+      )}
     </>
   );
 };
