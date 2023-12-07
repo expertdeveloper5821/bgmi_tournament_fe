49d073116c7cbb5408d21ed00cf2b7ddaf511e1a
devashish <devashish.sharma@technogetic.com>
2023-12-07 18:32:27 +0530

    added other functionality for css part only

--- src/Components/CommonComponent/Modal/AssignModal.tsx
+++ src/Components/CommonComponent/Modal/AssignModal.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from '@/styles/assign.module.scss';
 import Image from 'next/image';
 
@@ -11,51 +11,124 @@ interface DemoData {
   url: string;
   name: string;
 }
+
+const istItemArray: DemoData[] = [
+  {
+    id: 1,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Me',
+  },
+  {
+    id: 2,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Abhishek',
+  },
+  {
+    id: 3,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Akash',
+  },
+  {
+    id: 4,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Akhil',
+  },
+  {
+    id: 5,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Abhi',
+  },
+  {
+    id: 6,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'ak',
+  },
+  {
+    id: 7,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Nice',
+  },
+  {
+    id: 8,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Demo',
+  },
+  {
+    id: 9,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Me',
+  },
+  {
+    id: 10,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Me',
+  },
+  {
+    id: 11,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Me',
+  },
+  {
+    id: 12,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Me',
+  },
+  {
+    id: 13,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Me',
+  },
+  {
+    id: 14,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Me',
+  },
+  {
+    id: 15,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Me',
+  },
+  {
+    id: 16,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Me',
+  },
+  {
+    id: 17,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Me',
+  },
+  {
+    id: 18,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Me',
+  },
+  {
+    id: 19,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Me',
+  },
+  {
+    id: 20,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Me',
+  },
+  {
+    id: 21,
+    url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
+    name: 'Me',
+  },
+];
+
 const AssignModal = ({ onClickAssignHandler }: AssignModalPropsType) => {
   const modalRef = useRef<HTMLDivElement | null>(null);
+  const [selectedItem, setSelectedItem] = useState<number>(1);
+  const [listItemArray, setListItemArray] = useState<DemoData[] | []>(istItemArray);
 
-  const listItemArray: DemoData[] = [
-    {
-      id: 1,
-      url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
-      name: 'Me',
-    },
-    {
-      id: 2,
-      url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
-      name: 'Me',
-    },
-    {
-      id: 3,
-      url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
-      name: 'Me',
-    },
-    {
-      id: 4,
-      url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
-      name: 'Me',
-    },
-    {
-      id: 5,
-      url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
-      name: 'Me',
-    },
-    {
-      id: 6,
-      url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
-      name: 'Me',
-    },
-    {
-      id: 7,
-      url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
-      name: 'Me',
-    },
-    {
-      id: 8,
-      url: 'https://res.cloudinary.com/dh2lkjd5n/image/upload/v1701762915/dgmxu1pgkkde32kslmcg.png',
-      name: 'Me',
-    },
-  ];
+  const handleMouseEnter = (index: number) => {
+    setSelectedItem(index);
+    setListItemArray([...istItemArray]);
+  };
 
   const handleClick = (e) => {
     if (modalRef.current === e.target || modalRef.current?.contains(e.target)) {
@@ -86,7 +159,11 @@ const AssignModal = ({ onClickAssignHandler }: AssignModalPropsType) => {
         <div className={styles.list_container}>
           <div className={styles.list_items_container}>
             {listItemArray.map((item: DemoData) => (
-              <div className={styles.list_item} key={item.id}>
+              <div
+                onMouseEnter={() => handleMouseEnter(item.id)}
+                className={`${styles.list_item} ${item.id === selectedItem ? styles.selected : ''}`}
+                key={item.id}
+              >
                 <Image
                   src={item.url}
                   alt="profile image"
